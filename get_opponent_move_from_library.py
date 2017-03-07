3813d277eaad9f91e2dea88fdc0feab4d607c02c
Christian Gossett <cag343@drexel.edu>
2017-03-06 22:03:22 -0500

    Get opponent move from library

---
+++ get_opponent_move_from_library.py
@@ -0,0 +1,8 @@
+import collections
+
+def get_opponent_move_from_library():
+    print("Here is where the third party chess interface would ask the third party chess library for the oppponent's move")
+    print("Opponent's move will then be given to the third party chess interface")
+
+    return "Opponent initial move", "Opponent final move"
+
