d91d95ececdb1049dc88bea8749fe6c748b16c5e
Nikhil Rai Mehta <nrm62@drexel.edu>
2017-03-13 16:51:29 -0400

    The function for the LCD Display

---
+++ LCD Interface Function.py
@@ -0,0 +1,5 @@
+def display(first_line, second_line, third_line):
+first_line = print("Enter the text for the first line")
+second_line = print("Enter the text for the first line")
+third_line = print("Enter the text for the first line")
+
