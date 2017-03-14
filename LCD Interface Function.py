e4572dd09fcb837361a308a84378c2f6662cb962
Nikhil Rai Mehta <nrm62@drexel.edu>
2017-03-13 20:02:20 -0400

    Edited the name and added the comments suggested of the LCD Interface function

--- LCD Interface Function.py
+++
@@ -1,5 +0,0 @@
-def display(first_line, second_line, third_line):
-first_line = print("Enter the text for the first line")
-second_line = print("Enter the text for the first line")
-third_line = print("Enter the text for the first line")
-
---
+++ lcd_interface.py
@@ -0,0 +1,6 @@
+class LCDInterface():
+    def display(first_line, second_line, third_line):
+        first_line = print("Enter the text for the first line")
+        second_line = print("Enter the text for the second line")
+        third_line = print("Enter the text for the third line")
+
