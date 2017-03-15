c4ed232ab8416d2b35bf0420f83abb6ab7f4047c
Noah Huppert <noahhuppert@gmail.com>
2017-03-14 20:19:13 -0400

    fixed display fn in lcd_interface

--- lcd_interface.py
+++ lcd_interface.py
@@ -1,6 +1,6 @@
 class LCDInterface():
     def display(first_line, second_line, third_line):
-        first_line = print("Enter the text for the first line")
-        second_line = print("Enter the text for the second line")
-        third_line = print("Enter the text for the third line")
+        print(first_line)
+        print(second_line)
+        print(third_line)
 
