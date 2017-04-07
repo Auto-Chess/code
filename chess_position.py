50d49b907d914258f0da4c17381bff963744a6df
Michael <mjr425@drexel.edu>
2017-04-07 17:22:04 -0400

    chess position with raise

--- chess_position.py
+++ chess_position.py
@@ -9,12 +9,11 @@ class ChessPosition:
 
     @row.setter
     def row(self, value):
-        value = value.lower()
         if (value >= 1) and (value <= 8):
             self._row = value
         else:
             raise ValueError("Value must be between 1 and 8, was: {}".format(value))
-        self._row = value
+
 
 
     @property
