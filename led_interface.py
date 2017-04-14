e1d74f95758d59fd0e2a0aa65b22ca7c0bf3f400
cag343 <cagossett11@comcast.net>
2017-04-14 15:14:07 -0400

    Add files via upload
    Updated version of the led_interface.py using the ChessPosition class and it's variables for the position

--- led_interface.py
+++ led_interface.py
@@ -1,21 +1,22 @@
 import collections
+from chess_position import ChessPosition
 
 class LedInterface():
 
     def __init__(self):
-        pass
+        self.chess_pos = ChessPosition(self, col, row)
 
-    def turn_on_led(self, position):
+    def turn_on_led(self, col, row):
         print("Takes a chess coordinate pair (position) to turn on an LED to show initial position")
 
 
-    def turn_off_led(self, position):
+    def turn_off_led(self, col, row):
         print("Takes a chess coordinate pair (position) to turn off an LED")
 
 
-    def start_blinking_led(self, position, blink_interval):
+    def start_blinking_led(self, col, row, blink_interval):
         print("Makes the LED start blinking in order to show the user where to move the piece to")
 
 
-    def stop_blinking_led(self, position, blink_interval):
+    def stop_blinking_led(self, col, row, blink_interval):
         print("Makes the LED stop blinking")
