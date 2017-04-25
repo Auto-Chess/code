062bd9dc4ba019bafa4bf06d1a627913ce18d6dc
Jay Knauer <jkk73@drexel.edu>
2017-04-25 11:36:10 -0400

    Fixed accidental changes (again)

--- chess_library.py
+++ chess_library.py
@@ -1,23 +1,16 @@
-from chess_move import ChessMove
-from chess_position import ChessPosition
-
-import chess
-
-# This object is created purely for testing purposes as of now
-board = chess.Board()
+import collections
 
 
-# Give a ChessMove object to the third-party library
-def hand_off(move):
-    if isinstance(move, ChessMove):
-        board.parse_uci(move.__str__())
-    else:
-        raise TypeError("Parameter of hand_off must be of type ChessMove")
+class ChessLibrary():
+    def handOff(self, moveX, moveY):
 
+        sentence = 'Giving {} and {} to chess library.'.format(moveX, moveY)
+        print(sentence)
 
-# Asks Chess Library for a move, and returns it as a ChessMove object
-def get_move():
-    init = ChessPosition(0, 0)
-    final = ChessPosition(1, 1)
+    def getMove(self):
 
-    return ChessMove(init, final)
+        sentence = 'Asking library for AI move... Done. Returning (x), (y)'
+        print(sentence)
+        x = 0
+        y = 0
+        return x, y
