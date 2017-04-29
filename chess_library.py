04f4334f1ba2655a332cf00bd12105dfe4c45b25
Jay Knauer <jkk73@drexel.edu>
2017-04-28 20:41:11 -0400

    Library, move, and position updates

--- chess_library.py
+++ chess_library.py
@@ -1,23 +1,39 @@
 from chess_move import ChessMove
 from chess_position import ChessPosition
 
-import chess
+import chess.uci
 
-# This object is created purely for testing purposes as of now
+# This object is created purely for testing purposes as of now, but might exist permanently in this class later
 board = chess.Board()
 
+# Setup Stockfish engine
+engine = chess.uci.popen_engine("/stockfish_8_x32")
+engine.uci()
 
-# Give a ChessMove object to the third-party library
+
+# Give a ChessMove object to the third-party library, push it to board if ok
 def hand_off(move):
     if isinstance(move, ChessMove):
-        board.parse_uci(move.__str__())
+        try:
+            board.push_uci(move.__str__())
+            engine.position(board)
+        except ValueError:
+            print('\033[91m' + "Invalid move: " + move.__str__() + "!" + '\033[0m')
     else:
         raise TypeError("Parameter of hand_off must be of type ChessMove")
 
 
-# Asks Chess Library for a move, and returns it as a ChessMove object
+# Asks Chess Library for a move, pushes it to board, and returns it as a ChessMove object
 def get_move():
-    init = ChessPosition("a", 0)
-    final = ChessPosition("a", 0)
+    command = engine.go(movetime=2000, async_callback=True)
+    best_move, ponder = command.result()
+    bm = str(best_move)
+
+    # Converts UCI protocol to ChessPosition via splicing
+    init = ChessPosition(str(bm[:1]), int(bm[1:-2]))
+    final = ChessPosition(str(bm[2:-1]), int(bm[3:]))
+
+    board.push_uci(str(best_move))
+    engine.position(board)
 
     return ChessMove(init, final)
--- chess_position.py
+++ chess_position.py
@@ -4,27 +4,27 @@ class ChessPosition:
         self.row = row
 
     @property
-    def row(self):
-        return self.row
-
-    @row.setter
-    def row(self, value):
-        if (value >= 1) and (value <= 8):
-            self.row = value
-        else:
-            raise ValueError("Value must be between 1 and 8, was: {}".format(value))
-
-    @property
     def col(self):
-        return self.col
+        return self._col
 
     @col.setter
     def col(self, value):
         value = value.lower()
         if (value >= "a") and (value <= "h"):
-            self.col = value
+            self._col = value
         else:
             raise ValueError("Value must be between a and h, was: {}".format(value))
+
+    @property
+    def row(self):
+        return self._row
+
+    @row.setter
+    def row(self, value):
+        if (value >= 1) and (value <= 8):
+            self._row = value
+        else:
+            raise ValueError("Value must be between 1 and 8, was: {}".format(value))
 
     def __str__(self):
         return self.col + str(self.row)
