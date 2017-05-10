2030ecee9dd6cafcbcd1765d69e7e785aa803119
cag343 <cagossett11@comcast.net>
2017-05-10 14:24:51 -0400

    Fixed code
    Put setmode and setup in __init__ function and added time to second time.sleep

--- led_interface.py
+++ led_interface.py
@@ -6,29 +6,44 @@ from chess_move import ChessMove
 
 class LedInterface():
 
-    def __init__(self):
-       pass
+    def __init__(self, operation_mode="hardware"):
+        self.operation_mode = operation_mode
 
-    #Sets up the number system for the board and the output pin (location 12 on the diagram)
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(12, GPIO.OUT)
+        #Sets up the number system for the board and the output pin (location 12 on the diagram)
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(12, GPIO.OUT)
 
     def turn_on_led(self, position):
-        GPIO.output(12, 1)
+        if self.operation_mode == "hardware":
+            GPIO.output(12, 1)
+        else:
+            print("LED was turned on")
 
     def turn_off_led(self, position):
-        GPIO.output(12, 0)
+        if self.operation_mode == "hardware":
+            GPIO.output(12, 0)
+        else:
+            print("LED was turned off")
 
     def start_blinking_led(self, position, blink_interval):
-        for i in range(5)
+        if self.operation_mode == "hardware":
+            for i in range(5)
             GPIO.output(12, 1)
             time.sleep(1)
             GPIO.output(12, 0)
-            time.sleep
+            time.sleep(1)
+        else:
+            print("LED started blinking")
 
     def stop_blinking_led(self, position, blink_interval):
-        GPIO.output(12, 0)
+        if self.operation_mode == "hardware":
+            GPIO.output(12, 0)
+        else:
+            print("LED stopped blinking")
 
     def stop_all(self, position):
-        GPIO.output(12, 0)
+        if self.operation_mode == "hardware":
+            GPIO.output(12, 0)
+        else:
+            print("All LEDs were turned off")
 
