452baa3643cc93aea8a42bc06291fb50da27dd99
Noah Huppert <noahhuppert@gmail.com>
2017-05-10 00:01:01 -0400

    Cleaned up main file

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import collections
 from game_loop_entity import GameLoopEntity
 """
 Main File, run this file to start Auto Chess.
@@ -8,13 +7,8 @@ Run by typing:
 
 into your terminal in the directory of this file (main.py).
 This tells your computer to run the 'main.py' file with Python on your computer.
-
-Above you see some lines with 'import blablabla'.
-These ask Python to include other libraries that are either
-included in Python by default or downloaded from the internet.
 """
 
 # Game loop
 game_loop = GameLoopEntity()
-
 game_loop.run()
