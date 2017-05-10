f2b7dfe0de72a4288ccb127db3eda6d867d24296
Nikhil-RaiMehta <nmehta.rai@gmail.com>
2017-05-10 14:19:18 -0400

    LCD Basic Test File

---
+++ test_lcd_interface.py
@@ -0,0 +1,13 @@
+import unittest
+from chess_library import ChessLibrary
+from game_loop_entity import GameLoopEntity
+from chess_position import ChessPosition
+from chess_move import ChessMove
+from led_interface import LedInterface
+from mock import MagicMock
+from mock import patch
+from mock import call
+
+class TestLCDInerface(unittest.TestCase):
+    def test_display(self):
+        self.assert_equals(True,True)
