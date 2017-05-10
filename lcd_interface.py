282bba3689089c39fa815f20a03d69563040dc17
Nikhil-RaiMehta <nmehta.rai@gmail.com>
2017-05-10 14:29:35 -0400

    Adding Operation mode

--- lcd_interface.py
+++ lcd_interface.py
@@ -1,6 +1,17 @@
+
 class LCDInterface():
+    def __init__(self, operation_mode="hardware"):
+        self.operation_mode = operation_mode
+    
     def display(self, first_line, second_line):
-        print(first_line)
-        print(second_line)
+        if (self.operation_mode=="hardware"):
+            print("To hardware: {}".format(first_line))
+            print("To hardware: {}".format(second_line))
+        else:
+            print(first_line)
+            print(second_line)
+        
+
+ 
 
 
