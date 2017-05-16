f58580bba2028f56f6b5018ff3d4240ebc44dfb7
Noah Huppert <noahhuppert@gmail.com>
2017-05-16 19:24:14 -0400

    Added function to delete unused ChessMove models from db

--- webserver.py
+++ webserver.py
@@ -1,5 +1,6 @@
 import os
 import binascii
+from datetime import datetime, timedelta
 
 from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
@@ -16,11 +17,13 @@ class ChessBoard(db.Model):
 
     id = db.Column(db.Integer, primary_key=True, nullable=False)
     secret = db.Column(db.String(64), nullable=False)
+    last_used = db.Column(db.DateTime, nullable=False)
 
     chess_moves = db.relationship('ChessMove', back_populates='chess_board')
 
     def __init__(self):
         self.secret = binascii.hexlify(os.urandom(32)).decode("utf-8")[:64]
+        self.last_used = datetime.today()
 
     def __str__(self):
         return "<ChessBoard id={}, secret={}, chess_moves.len={}>".format(self.id, self.secret, len(self.chess_moves))
@@ -32,8 +35,32 @@ class ChessBoard(db.Model):
         return {
             'id': self.id,
             'secret': self.secret,
+            'last_used': self.last_used,
             'chess_moves': self.chess_moves
         }
+
+    @staticmethod
+    def clean_old():
+        # Get all ChessBoards that haven't been used in a day, and delete
+        old_date = datetime.today() - timedelta(days=1)
+        print("Looking for ChessBoards older than {} to remove".format(old_date))
+
+        old = ChessBoard.query.filter(ChessBoard.last_used < old_date).all()
+        for board in old:
+            print("Deleting board: {}".format(board))
+            db.session.delete(board)
+
+            # Delete moves
+            i = 0
+            for move in board.chess_moves:
+                db.session.delete(move)
+                i += 1
+
+            if i > 0:
+                print("    Delete {} child ChessMoves".format(i))
+
+        print("Committing deletion")
+        db.session.commit()
 
 class ChessMove(db.Model):
     __tablename__ = 'chess_moves'
@@ -68,6 +95,16 @@ class ChessMove(db.Model):
     def __repr__(self):
         return str(self)
 
+    def serialize(self):
+        return {
+            'id': self.id,
+            'chess_board_id': self.chess_board_id,
+            'inital_col': self.initial_col,
+            'inital_row': self.inital_row,
+            'final_col': self.final_col,
+            'final_row': self.final_row
+        }
+
 # -- -- Create
 db.create_all()
 
@@ -91,6 +128,9 @@ def api_register():
     })
 
 
+
+
 # Run if not being included
 if __name__ == "__main__":
+    ChessBoard.clean_old()
     app.run()
