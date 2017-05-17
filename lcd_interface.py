bc54f36b1bb51ec997423ccaa1431bdf0cf00436
Nikhil-RaiMehta <nmehta.rai@gmail.com>
2017-05-17 12:11:30 -0400

    Update lcd.py
    This is the new code for the lcd display

--- lcd_interface.py
+++ lcd_interface.py
@@ -1,7 +1,18 @@
+import serial
 
 class LCDInterface():
+
     def __init__(self, operation_mode="hardware"):
         self.operation_mode = operation_mode
+        self.lcdCom = serial.Serial
+            (
+             port="/dev/serial0",
+             baudrate=9600,
+             parity=serial.PARITY_NONE,
+             stopbits=serial.STOPBITS_ONE,
+             bytesize=serial.EIGHTBITS,
+             timeout=1
+            )
     
     def display(self, first_line, second_line):
         if (self.operation_mode=="hardware"):
@@ -10,8 +21,16 @@ class LCDInterface():
         else:
             print(first_line)
             print(second_line)
-        
 
- 
+    def clear():
+        self.lcdCom.write(b"\xFE\x01")
+        self.lcdCom.write(bytes("                ", "ASCII"))
+        self.lcdCom.write(bytes("                ", "ASCII"))
 
+    def dPrint():
+        self.lcdCom.write(b"\xFE\x01")
+        self.lcdCom.write(bytes("Line 1", "ASCII"))
+        self.lcdCom.write(bytes("Line 2", "ASCII"))
 
+    def close():
+        self.lcdCom.close()
