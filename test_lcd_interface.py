91200583af9284d0a12df6b34fbf4f9630bb9175
Nikhil Rai Mehta <nrm62@drexel.edu>
2017-05-24 07:51:55 -0400

    Added first and second line input length limit

--- test_lcd_interface.py
+++ test_lcd_interface.py
@@ -11,3 +11,9 @@ from mock import call
 class TestLCDInerface(unittest.TestCase):
     def test_display(self):
         self.assertEquals(True,True)
+
+if len(first_line) > 16:
+    raise ValueError("First line of text can not be more than 16 characters")
+
+if len(second_line) > 16:
+    raise ValueError("First line of text can not be more than 16 characters")
