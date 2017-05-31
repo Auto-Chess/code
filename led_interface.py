66b7f8bdf87768b6317d9ed5de3c81387dc018e2
cag343 <cagossett11@comcast.net>
2017-05-31 10:35:49 -0400

    Add files via upload
    Copied over the matrix control functions. Still need to fix original 4 functions

--- led_interface.py
+++ led_interface.py
@@ -1,11 +1,12 @@
 import collections
 import time
+import RPi.GPIO as GPIO
 from chess_position import ChessPosition
 from chess_move import ChessMove
 
 class LedInterface():
 
-    def __init__(self, operation_mode="hardware"):
+    def __init__(self, operation_mode="hardware", lows, highs):
         self.operation_mode = operation_mode
 
         if self.operation_mode == "hardware":
@@ -13,9 +14,94 @@ class LedInterface():
         else:
             from fake_gpio import GPIO
 
-        #Sets up the number system for the board and the output pin (location 12 on the diagram)
+
+        self.lows = lows
+        self.highs = highs
+
+        self.on = []
+
+    def setup(self):
         GPIO.setmode(GPIO.BCM)
-        GPIO.setup(12, GPIO.OUT)
+
+        # Init all pins
+        for pin in self.lows:
+            GPIO.setup(pin, GPIO.OUT)
+
+        for pin in self.highs:
+            GPIO.setup(pin, GPIO.OUT)
+
+    def reset(self):
+        # Set all to low
+        for pin in self.lows:
+            GPIO.output(pin, 0)
+
+        for pin in self.highs:
+            GPIO.output(pin, 0)
+
+    def turn_all_to_except(self, value, pin_list, exception_i):
+        if exception_i > len(pin_list) - 1:
+            raise ValueError(
+                "exception_i can not be bigger than pin_list size: exception_i: {}, size: {}".format(exception_i,
+                                                                                                     len(pin_list) - 1))
+
+        i = 0
+        for pin in pin_list:
+            if i != exception_i:
+                GPIO.output(pin, value)
+            i += 1
+
+    def cleanup(self):
+        self.reset()
+        GPIO.cleanup()
+
+    def add_on(self, x, y):
+        # Check if x and y in bounds
+        if x > len(self.highs) - 1:
+            raise ValueError("x can not be bigger than high length")
+        if y > len(self.lows) - 1:
+            raise ValueError("y can not be bigger than lows length")
+
+        # Check if already on
+        if [x, y] in self.on:
+            raise ValueError("Already added to on")
+
+        # Otherwise add
+        self.on.append([x, y])
+
+    def remove_on(self, x, y):
+        # Check if in on list
+        if [x, y] in self.on:
+            self.on.remove([x, y])
+        else:
+            raise IndexError("Not in ons")
+
+    def run(self, run_max=None):
+        run_count = 1
+        while run_max is None or run_count < run_max:
+            try:
+                # For each coordinate to turn on
+                print("Running Matrix for the {}th time".format(run_count))
+                for coord in self.on:
+                    x = coord[0]
+                    y = coord[1]
+
+                    high_pin = self.highs[x]
+                    low_pin = self.lows[y]
+
+                    print("    ({}, {}) => pins: (high_pin, low_pin) = ({}, {})".format(x, y, high_pin, low_pin))
+
+                    GPIO.output(high_pin, 1)
+                    self.turn_all_to_except(0, self.highs, x)
+
+                    GPIO.output(low_pin, 0)
+                    self.turn_all_to_except(1, self.lows, y)
+
+                    run_count += 1
+                    time.sleep(0.1 / len(self.on))
+
+            except KeyboardInterrupt as e:
+                print("Stopping")
+                return;
 
     def turn_on_led(self, position):
         if self.operation_mode == "hardware":
@@ -51,3 +137,5 @@ class LedInterface():
         else:
             print("All LEDs were turned off")
 
+matrix = LEDMatrix(lows=[26, 16], highs=[20, 21])
+matrix.setup()
