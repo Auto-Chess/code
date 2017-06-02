300e9d9a1ddc515e14908eba6cc897211633faa2
Jay <jkk73@drexel.edu>
2017-06-02 17:39:39 -0400

    Added documentation to chess_library

--- chess_library.py
+++ chess_library.py
@@ -11,7 +11,13 @@ class ChessLibrary():
         self.engine = chess.uci.popen_engine("stockfish_8")
         self.engine.uci()
 
-    # Give a ChessMove object to the third-party library, push it to board if ok
+    '''Gives a ChessMove object to the third-party library, which is then pushed to the board if ok
+    Arg:
+        - move: A ChessMove object which is then translated into a UCI move
+    Raises:
+        - ValueError: Raised if the move given is illegal
+        - TypeError: Raised if parameter 'move' is not of type ChessMove
+    '''
     def hand_off(self, move):
         if isinstance(move, ChessMove):
             try:
@@ -22,26 +28,35 @@ class ChessLibrary():
         else:
             raise TypeError("Parameter of hand_off must be of type ChessMove")
 
-    # Asks Chess Library for a move, pushes it to board, and returns it as a ChessMove object
+    '''Asks Chess Library for a move, pushes it to board, and returns it as a ChessMove object
+    Returns:
+        - A ChessMove object which shows where the chess engine decided to move
+    '''
     def get_move(self):
         command = self.engine.go(movetime=2000, async_callback=True)
         best_move, ponder = command.result()
         bm = str(best_move)
 
-        # Converts UCI protocol to ChessPosition via splicing
+        # Converts UCI protocol to ChessPosition via slicing
         init = ChessPosition(str(bm[:1]), int(bm[1:-2]))
         final = ChessPosition(str(bm[2:-1]), int(bm[3:]))
 
         return ChessMove(init, final)
 
-    # Self explanatory
+    '''Evaluates whether the game is over or not
+    Returns:
+        - A boolean which is true if the game has ended, false if not
+    '''
     def is_game_over(self):
         return self.board.is_game_over()
       
-    # Sets difficulty. Must be between 0 and 20, 20 being hardest
+    '''Sets difficulty. Must be between 0 and 20, 20 being hardest
+    Raises:
+        - ValueError: Raised if the difficulty is not within legal boundaries 
+    '''
     def set_difficulty(self, difficulty):
         if difficulty < 0 or difficulty > 20:
-            raise ValueError("Difficulty must be between 0 and 20")
+            raise ValueError("Difficulty must be between 0 and 20 inclusive")
         else:
             new_options = {'Skill Level': difficulty}
             self.engine.setoption(new_options)
