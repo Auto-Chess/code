978e46e81af7726206c94a125df385b8854794e7
cag343 <cagossett11@comcast.net>
2017-06-04 19:34:10 -0400

    Add files via upload
    Finalized LED code

--- led_interface.py
+++ led_interface.py
@@ -1,8 +1,8 @@
 import collections
 import time
 import RPi.GPIO as GPIO
+import threading
 from chess_position import ChessPosition
-from chess_move import ChessMove
 
 class LedInterface():
 
@@ -14,10 +14,18 @@ class LedInterface():
         else:
             from fake_gpio import GPIO
 
-
         self.lows = lows
         self.highs = highs
 
+        self.on = []
+        self.on_lock = threading.Lock()
+
+        self.blinking = []
+        self.last_blinks = {}
+        self.blink_intervals = {}
+        self.blinking_lock = threading.Lock()
+
+    def all_off(self):
         self.on = []
 
     def setup(self):
@@ -38,104 +46,99 @@ class LedInterface():
         for pin in self.highs:
             GPIO.output(pin, 0)
 
-    def turn_all_to_except(self, value, pin_list, exception_i):
-        if exception_i > len(pin_list) - 1:
-            raise ValueError(
-                "exception_i can not be bigger than pin_list size: exception_i: {}, size: {}".format(exception_i,
-                                                                                                     len(pin_list) - 1))
 
+    def cleanup(self):
+        self.reset()
+        GPIO.cleanup()
+
+    def turn_all_to_except(self, value, pin_list, exception_i):
         i = 0
         for pin in pin_list:
             if i != exception_i:
                 GPIO.output(pin, value)
             i += 1
 
-    def cleanup(self):
-        self.reset()
-        GPIO.cleanup()
-
-    def add_on(self, x, y):
+    def turn_on_led(self, position):
         # Check if x and y in bounds
+        x = position.call_to_int()
+        y = position.row-1
+
         if x > len(self.highs) - 1:
             raise ValueError("x can not be bigger than high length")
         if y > len(self.lows) - 1:
             raise ValueError("y can not be bigger than lows length")
 
-        # Check if already on
-        if [x, y] in self.on:
-            raise ValueError("Already added to on")
-
         # Otherwise add
-        self.on.append([x, y])
+        with self.on_lock:
+            self.on.append([x, y])
 
-    def remove_on(self, x, y):
+    def turn_off_led(self, position):
         # Check if in on list
+        x = position.call_to_int()
+        y = position.row - 1
+
         if [x, y] in self.on:
-            self.on.remove([x, y])
+            with self.on_lock:
+                self.on.remove([x, y])
         else:
             raise IndexError("Not in ons")
 
-    def run(self, run_max=None):
-        run_count = 1
-        while run_max is None or run_count < run_max:
-            try:
-                # For each coordinate to turn on
-                print("Running Matrix for the {}th time".format(run_count))
-                for coord in self.on:
-                    x = coord[0]
-                    y = coord[1]
+    def start_blinking_led(self, position, interval):
+        x = position.call_to_int()
+        y = position.row - 1
+
+        with self.blinking_lock:
+            self.blinking.append([x,y])
+            k = "{}{}".format(x,y)
+            self.last_blinks[k] = time.time()
+            self.blink_intervals[k] = interval
+
+    def stop_all(self):
+        with self.on_lock:
+            self.on = []
+
+        with self.blinking_lock:
+            self.blinking = []
+
+    def run(self):
+        while True
+            with self.blinking_lock
+                for pos in self.blinking:
+                    x = pos[0]
+                    y = pos[1]
+                    k = "{}{}".format(x, y)
+                    dt = time.time()-self.last_blinks[k]
+                    i = self.blink_intervals[k]
+
+                    if dt >= 0 and dt < i:
+                        with self.on_lock:
+                            self.on.append([x,y])
+                    elif dt >= i and dt < i*2:
+                        with self.on_lock:
+                            self.on.remove([x,y])
+                    else:
+                        self.last_blinks[k] = time.time()
+
+            with self.on_lock:
+                for pos in self.on:
+                    x = pos[0]
+                    y = pos[1]
 
                     high_pin = self.highs[x]
-                    low_pin = self.lows[y]
+                    low_pin=self.lows[y]
 
-                    print("    ({}, {}) => pins: (high_pin, low_pin) = ({}, {})".format(x, y, high_pin, low_pin))
-
-                    GPIO.output(high_pin, 1)
+                    GPIO.output(high_pin,1)
                     self.turn_all_to_except(0, self.highs, x)
 
-                    GPIO.output(low_pin, 0)
+                    GPIO.output(low_pin,0)
                     self.turn_all_to_except(1, self.lows, y)
 
-                    run_count += 1
-                    time.sleep(0.1 / len(self.on))
+                    divisor = len(self.on)
+                    if divisor == 0:
+                        divisor = 1
 
-            except KeyboardInterrupt as e:
-                print("Stopping")
-                return;
+                    time.sleep(0.01 / divisor)
 
-    def turn_on_led(self, position):
-        if self.operation_mode == "hardware":
-            GPIO.output(12, 1)
-        else:
-            print("LED was turned on")
-
-    def turn_off_led(self, position):
-        if self.operation_mode == "hardware":
-            GPIO.output(12, 0)
-        else:
-            print("LED was turned off")
-
-    def start_blinking_led(self, position, blink_interval):
-        if self.operation_mode == "hardware":
-            for i in range(5)
-            GPIO.output(12, 1)
-            time.sleep(1)
-            GPIO.output(12, 0)
-            time.sleep(1)
-        else:
-            print("LED started blinking")
-
-    def stop_blinking_led(self, position, blink_interval):
-        if self.operation_mode == "hardware":
-            GPIO.output(12, 0)
-        else:
-            print("LED stopped blinking")
-
-    def stop_all(self, position):
-        if self.operation_mode == "hardware":
-            GPIO.output(12, 0)
-        else:
-            print("All LEDs were turned off")
-
-matrix = LEDMatrix(lows=[26, 16], highs=[20, 21])
-matrix.setup()
+    def start_run_in_thread(self):
+        thread = threading.Thread(target= self.run)
+        thread.start()
